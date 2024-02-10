27eff9efa63b2256dae667cbe544677d9d0637b7
Daniel Estrella Couto <estrellacouto05@gmail.com>
2024-02-10 17:54:26 -0300

    Atualização 10/02

--- js/validaIdade.js
+++ js/validaIdade.js
@@ -1,11 +1,10 @@
 export default function ehMaiorDeIdade (campo) {
     const dataNascimento = new Date(campo.value)
-    validaIdade(dataNascimento)
-
-    console.log(validaIdade(dataNascimento))
+    if (!validaIdade(dataNascimento)) {
+        campo.setCustomValidity('Não é Maior de Idade!!!')
+    }
 
 }
-
 function validaIdade (data){
     const dataAtual = new Date()
     const dataMais18 = new Date (data.getUTCFullYear()+18, data.getUTCMonth(), data.getUTCDate())
